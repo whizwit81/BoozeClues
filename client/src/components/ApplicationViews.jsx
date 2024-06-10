117d7bc21f919698e94035fd52a9a876e2a99efc
Rob Witkowski <rob_witkowski@hotmail.com>
2024-06-10 10:18:25 -0400

    initial commit

---
+++ client/src/components/ApplicationViews.jsx
@@ -0,0 +1,68 @@
+import { Route, Routes } from "react-router-dom";
+import Bikes from "./bikes/Bikes";
+import { AuthorizedRoute } from "./auth/AuthorizedRoute";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import WorkOrderList from "./WorkOrders/WorkOrderList.jsx";
+import CreateWorkOrder from "./WorkOrders/CreateWorkOrder.jsx";
+import UserProfileList from "./userprofiles/UserProfileList.jsx";
+
+export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route
+          index
+          element={
+            <AuthorizedRoute loggedInUser={loggedInUser}>
+              {/* <Bikes /> */}
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="bikes"
+          element={
+            <AuthorizedRoute loggedInUser={loggedInUser}>
+              {/* <Bikes /> */}
+            </AuthorizedRoute>
+          }
+        />
+        <Route path="workorders">
+    <Route
+    index
+    element={
+        <AuthorizedRoute loggedInUser={loggedInUser}>
+            {/* <WorkOrderList /> */}
+        </AuthorizedRoute>
+    }
+    />
+    <Route
+    path="create"
+    element={
+        <AuthorizedRoute loggedInUser={loggedInUser}>
+            {/* <CreateWorkOrder /> */}
+        </AuthorizedRoute>
+    }
+    />
+</Route>
+        <Route
+          path="employees"
+          element={
+            <AuthorizedRoute roles={["Admin"]} loggedInUser={loggedInUser}>
+              {/* <UserProfileList /> */}
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={<Login setLoggedInUser={setLoggedInUser} />}
+        />
+        <Route
+          path="register"
+          element={<Register setLoggedInUser={setLoggedInUser} />}
+        />
+      </Route>
+      <Route path="*" element={<p>Whoops, nothing here...</p>} />
+    </Routes>
+  );
+}
