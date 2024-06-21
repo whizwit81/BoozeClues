a20ddf0852af80c90f7fe9acf46d8ba13d6aac2d
Rob Witkowski <rob_witkowski@hotmail.com>
2024-06-20 20:03:21 -0400

    css changes

--- client/src/components/recipes/ViewRecipes.jsx
+++ client/src/components/recipes/ViewRecipes.jsx
@@ -23,17 +23,15 @@ export const ViewCocktailRecipes = ({loggedInUser}) => {
             <h2 className="text-center">Cocktail Recipes</h2>
             <Row className="justify-content-center align-items-center cocktail-recipes-list">
                 {recipes.map(recipe => (
-                    <Col md="4" sm="6" xs="12" key={recipe.id} className="cocktail-recipe">
-                        <Card className="h-100" d-flex flex-row align-items-center> 
+                    <Col lg="3" md="4" sm="6" xs="12" key={recipe.id} className="cocktail-recipe">
+                        <Card className="h-100"> 
+                            <CardImg src={recipe.image} alt={recipe.name} className="recipe-image"/>
                             <CardBody className="d-flex flex-column justify-content-between align-items-center">
                                 <CardTitle tag="h3" className="text-center">{recipe.name}</CardTitle>
-                            </CardBody>
-                            <img src={recipe.image} alt={recipe.name}/>
-                            <CardBody>
                                 <Button className="custom-button" onClick={() => handleDetails(recipe.id)}>
                                     View Recipe
                                 </Button>
-                        </CardBody>
+                           </CardBody>
                         </Card>
                     </Col>
                 ))}
